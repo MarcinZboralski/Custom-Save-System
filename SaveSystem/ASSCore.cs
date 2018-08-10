bad3e1e42462bf06db2c254a1c4b39665569e857
Marcin Zboralski <32996203+MarcinZboralski@users.noreply.github.com>
2018-08-10 12:39:26 +0200

    Dodaj pliki projektów.

---
+++ SaveSystem/ASSCore.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AdwancedSaveSystem.SaveSystem
+{
+    [Serializable]
+    public class ASSCore
+    {
+        public static ASSCore istance;
+        public Stream stream;
+        
+        public ASSDictonary<string, object> SaveDictonary;
+
+        private BinaryFormatter binaryFormatter = new BinaryFormatter();
+        private XmlSerializer xmlSerializer = new XmlSerializer(typeof(ASSDictonary<string,object>));
+        private ASSDictonary<string, object> loadeDictonary;
+
+        public ASSCore()
+        {
+            istance = this;
+        }
+
+        public void Serialize(FileStream stream,TypeOfSerializaton typeOfSerializaton,ASSDictonary<string,object> toSave)
+        {
+            using (stream)
+            {
+                switch (typeOfSerializaton)
+                {
+                    case TypeOfSerializaton.Binary:
+                        binaryFormatter.Serialize(stream,toSave);
+                        break;
+                    case TypeOfSerializaton.XML:
+                        xmlSerializer.Serialize(stream,toSave);
+                        break;
+                }
+            }
+        }
+
+        public void Deserialize(FileStream stream,TypeOfSerializaton typeOfSerializaton,out ASSDictonary<string,object> loaded)
+        { 
+            using (stream)
+            {
+               
+                switch (typeOfSerializaton)
+                {
+                    case TypeOfSerializaton.Binary:
+                        loadeDictonary = (ASSDictonary<string,object>)binaryFormatter.Deserialize(stream);
+                        break;
+                    case TypeOfSerializaton.XML:
+                        loadeDictonary = (ASSDictonary<string,object>)binaryFormatter.Deserialize(stream);
+                        break;
+                }
+            }
+
+            loaded = loadeDictonary;
+        }
+
+        public void SerializeXML(string path)
+        {
+            using (stream = new FileStream(path,FileMode.CreateNew))
+            {
+                xmlSerializer.Serialize(stream,SaveDictonary);
+            }
+        }
+
+        public void SerializeXML(string path,ASSDictonary<string,object> toSave)
+        {
+            using (stream = new FileStream(path,FileMode.CreateNew))
+            {
+                xmlSerializer.Serialize(stream,toSave);
+            }
+        }
+
+        public void DeserializeXML(string path)
+        {
+            using (stream = new FileStream(path,FileMode.Open))
+            {
+                SaveDictonary = (ASSDictonary<string,object>)xmlSerializer.Deserialize(stream);
+            }
+        }
+
+        public void DeserializeXML(string path,out ASSDictonary<string,object> loadedFile)
+        {
+            using (stream = new FileStream(path,FileMode.Open))
+            {
+                loadedFile = (ASSDictonary<string,object>)xmlSerializer.Deserialize(stream);
+            }
+        }
+
+        public void SerializeBinary(string path)
+        {
+            using (stream = new FileStream(path,FileMode.CreateNew))
+            {
+                binaryFormatter.Serialize(stream,SaveDictonary);
+            }
+        }
+
+        public void SerializeBinary(string path,ASSDictonary<string,object> toSave)
+        {
+            using (stream = new FileStream(path,FileMode.CreateNew))
+            {
+                binaryFormatter.Serialize(stream,toSave);
+            }
+        }
+
+        public void DeserializeBinary(string path)
+        {
+            using (stream = new FileStream(path,FileMode.Open))
+            {
+                SaveDictonary = (ASSDictonary<string,object>)binaryFormatter.Deserialize(stream);
+            }
+        }
+
+        public void DeserializeBinary(string path,out ASSDictonary<string,object> loadedFile)
+        {
+            using (stream = new FileStream(path,FileMode.Open))
+            {
+                loadedFile = (ASSDictonary<string,object>)binaryFormatter.Deserialize(stream);
+            }
+        }
+    }
+
+    public enum TypeOfSerializaton
+    {
+        Binary,
+        XML
+    }
+}
---
+++ SaveSystem/ASSDictonary.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdwancedSaveSystem.SaveSystem
+{
+    [Serializable]
+    public class ASSDictonary<T,T1>
+    {
+        private T[] tArray;
+        private T1[] t1Array;
+        private int count = 0;
+
+        
+        [NonSerialized]private T1 targetValue;
+        [NonSerialized]private T targetKey;
+
+        [NonSerialized]
+        private Dictionary<T, T1> thisDictionary;
+
+        
+        public int Lenght
+        {
+            get { return count; }
+        }
+
+        public T[] Keys
+        {
+            get { return tArray; }
+
+        }
+
+        public T1[] Values
+        {
+            get { return t1Array; }
+        }
+
+        public ASSDictonary()
+        {
+        }
+
+        public ASSDictonary(ASSDictonary<T,T1> dictonary)
+        {
+            tArray = dictonary.Keys;
+            t1Array = dictonary.Values;
+            count = dictonary.Lenght;
+        }
+
+        public ASSDictonary(T[] key,T1[] value,int lenght)
+        {
+            if (Equals(Equals(key,value),lenght))
+            {
+                tArray = key.ToArray();
+                t1Array = value.ToArray();
+                count = lenght;
+
+            }
+            else
+            {
+                new Exception("Arrays length and length is not equals");
+            }
+        }
+
+        public void Add(T key, T1 value)
+        {
+            tArray[count] = key;
+            t1Array[count] = value;
+            count++;
+            UpdateThisDictonary();
+        }
+
+        public T1 GetValue(T key)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (tArray[i].Equals(key))
+                {
+                    targetValue = t1Array[i];
+                    break;
+                }
+            }
+            return targetValue;
+        }
+
+        public T GetKey(T1 value)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (tArray[i].Equals(value))
+                {
+                    targetKey = tArray[i];
+                    break;
+                }
+            }
+            return targetKey;
+        }
+
+        public Dictionary<T, T1> ToDictionary()
+        {
+            Dictionary<T,T1> dic =  new Dictionary<T, T1>();
+
+            for (int i = 0; i < count; i++)
+            {
+                dic.Add(tArray[i],t1Array[i]);
+            }
+
+            return dic;
+        }
+
+        public ASSDictonary<T,T1> ToASSDictonary()
+        {
+            return this;
+        }
+
+        private void UpdateThisDictonary()
+        {
+            thisDictionary = ToDictionary();
+        }
+    }
+}
